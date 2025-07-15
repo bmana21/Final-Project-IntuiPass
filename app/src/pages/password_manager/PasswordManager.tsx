19ca4af6f84d9a7b94f70b8e0477fd69de50cc6f
bmana21 <bmana21@freeuni.edu.ge>
2025-07-15 23:11:25 +0400

    feat: Add back button and PasswordManager.css for password manager page

--- app/src/pages/password_manager/PasswordManager.tsx
+++ app/src/pages/password_manager/PasswordManager.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-// import { useNavigation } from '../../components/AppRouter';
+import {useNavigation} from "../../components/AppRouter.tsx";
 
 const PasswordManager: React.FC = () => {
-    return <></>;
+    // @ts-ignore
+    const { _navigateTo, _getRouteParams, goBack } = useNavigation();
+
+    return (
+        <div className="popup-container">
+            <div className="header">
+                <button className="back-button" onClick={goBack}>
+                    ← Back
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default PasswordManager;
